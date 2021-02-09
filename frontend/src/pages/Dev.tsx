72704a8cc02ee6108cd63c4fab7b41d9bb40070f
Filipe Ventura <54460658+fcventura02@users.noreply.github.com>
2021-02-09 11:42:13 -0300

    test vercel

---
+++ frontend/src/pages/Dev.tsx
@@ -0,0 +1,14 @@
+import Head from "next/head";
+
+export default function Dev() {
+  return (
+    <>
+      <Head>
+        <title>Dev Ventura</title>
+      </Head>
+      <div>
+          <h1>Hello</h1>
+      </div>
+    </>
+  );
+}
